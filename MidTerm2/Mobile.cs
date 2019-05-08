510eeae38b79cf5879f668ecf7de6da2b4b7b31c
Matas <Matas@DESKTOP-2R5H11A>
2019-05-08 13:38:03 +0300

    lolo

---
+++ MidTerm2/Mobile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm2
+{
+    class Mobile
+    {
+        public string name { get; set; }
+        public string model { get; set; }
+        public double price { get; set; }
+
+        public Mobile() {
+            name = "";
+            model = "";
+            price = 0;
+        }
+
+        public Mobile(string name, string model, double price) {
+            this.name = name;
+            this.model = model;
+            this.price = price;
+        }
+    }
+}
